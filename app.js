7535de465bc0115ba55aa8b651c08ddf2f3939e4
Carolina Jung do Amaral <carolina.amaral@pmpa.ad>
2025-02-01 19:00:32 -0300

    feat: criação da rota de produtos e métodos get para lista de produtos e para pesquisa de produto por id

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var apiRouterV1 = require('./routes/apiRouterV1');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v1', apiRouterV1);
 
 module.exports = app;
--- routes/apiRouterV1.js
+++ routes/apiRouterV1.js
@@ -1,9 +1,75 @@
 var express = require('express');
-var router = express.Router();
+var apiRouterV1 = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var produtos = [
+  {
+    "id": 1,
+    "descricao": "Camiseta",
+    "marca": "Nike",
+    "preco": 79.90
+  },
+  {
+    "id": 2,
+    "descricao": "Calça Jeans",
+    "marca": "Zara",
+    "preco": 199.90
+  },
+  {
+    "id": 3,
+    "descricao": "Jaqueta de Couro",
+    "marca": "Levi's",
+    "preco": 349.90
+  },
+  {
+    "id": 4,
+    "descricao": "Tênis Esportivo",
+    "marca": "Adidas",
+    "preco": 299.90
+  },
+  {
+    "id": 5,
+    "descricao": "Blusa de Moletom",
+    "marca": "Puma",
+    "preco": 159.90
+  },
+  {
+    "id": 6,
+    "descricao": "Vestido Casual",
+    "marca": "H&M",
+    "preco": 129.90
+  },
+  {
+    "id": 7,
+    "descricao": "Bermuda",
+    "marca": "Tommy Hilfiger",
+    "preco": 149.90
+  },
+  {
+    "id": 8,
+    "descricao": "Boné",
+    "marca": "New Era",
+    "preco": 99.90
+  }
+]
+
+apiRouterV1.get('/produtos', function (req, res, next) {
+  res.json(produtos);
 });
 
-module.exports = router;
+apiRouterV1.get('/produtos/:id', function (req, res, next) {
+  let id = req.params.id;
+  if (id) {
+    idInt = Number.parseInt(id);
+    let idx = produtos.findIndex(produto => produto.id === idInt)
+    if (idx >= 0) {
+      res.json(produtos[idx]);
+    } else {
+      res.status('404').json({ message: 'Produto não encontrado.' })
+    }
+  } else {
+    res.status('404').json({ message: 'Produto não encontrado.' })
+  }
+
+});
+
+module.exports = apiRouterV1;
