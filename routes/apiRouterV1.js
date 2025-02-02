082483acb1dad3eba8eb8b723ae96c1af876a021
Carolina Jung do Amaral <carolina.amaral@pmpa.ad>
2025-02-02 10:29:42 -0300

    feat: criados os métodos post, put e delete de produtos

--- routes/apiRouterV1.js
+++ routes/apiRouterV1.js
@@ -59,7 +59,7 @@ apiRouterV1.get('/produtos', function (req, res, next) {
 apiRouterV1.get('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   if (id) {
-    idInt = Number.parseInt(id);
+    const idInt = Number.parseInt(id);
     let idx = produtos.findIndex(produto => produto.id === idInt)
     if (idx >= 0) {
       res.json(produtos[idx]);
@@ -70,6 +70,51 @@ apiRouterV1.get('/produtos/:id', function (req, res, next) {
     res.status('404').json({ message: 'Produto não encontrado.' })
   }
 
+});
+
+apiRouterV1.post('/produtos', function (req, res, next) {
+  let produto = req.body;
+  let newId = Math.max(...produtos.map(produto => produto.id)) + 1;
+
+  produto.id = newId
+  produtos.push(produto)
+
+  res.status(201).json({ message: 'Produto inserido com sucesso.', data: { id: newId } });
+});
+
+apiRouterV1.delete('/produtos/:id', function (req, res, next) {
+  let id = req.params.id;
+  if (id) {
+    const idInt = Number.parseInt(id);
+    let idx = produtos.findIndex(produto => produto.id === idInt)
+    if (idx >= 0) {
+      produtos.splice(idx, 1)
+      res.status('200').json({ message: 'Produto excluído com sucesso.' })
+    } else {
+      res.status('404').json({ message: 'Produto não encontrado.' })
+    }
+  } else {
+    res.status('404').json({ message: 'Produto não encontrado.' })
+  }
+});
+
+apiRouterV1.put('/produtos/:id', function (req, res, next) {
+  let id = req.params.id;
+  const produto = req.body
+  if (id) {
+    const idInt = Number.parseInt(id);
+    let idx = produtos.findIndex(produto => produto.id === idInt)
+    if (idx >= 0) {
+      produtos[idx].descrucao = produto.descricao
+      produtos[idx].marca = produto.marca
+      produtos[idx].preco = produto.preco
+      res.status('200').json({ message: 'Produto alterado com sucesso.', data: { produto: produtos[idx] } })
+    } else {
+      res.status('404').json({ message: 'Produto não encontrado.' })
+    }
+  } else {
+    res.status('404').json({ message: 'Produto não encontrado.' })
+  }
 });
 
 module.exports = apiRouterV1;
